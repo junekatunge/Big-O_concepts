3cb5073fd25378ed88ffdb81fb810601db36c923
junekatunge <jkatunge13@gmail.com>
2022-11-04 15:28:14 +0300

    i am amazing

--- questions/palindrome.py
+++ questions/palindrome.py
@@ -1,3 +1,7 @@
+# A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+# Given a string s, return true if it is a palindrome, or false otherwise.
+
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         l,r = 0, len(s) - 1
@@ -12,12 +16,7 @@ class Solution:
             l = l + 1
             r = r - 1
         return True
-                
-        
-        
-        
-        
-        
+            
     def alphanum(self,c):
         return (ord('A') <= ord(c) <= ord('Z') or
                 ord('a') <= ord(c) <= ord('z') or
