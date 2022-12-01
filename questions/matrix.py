42ce9bda01af25a634a42b20b3968a9fefe4359f
junekatunge <jkatunge13@gmail.com>
2022-12-01 17:51:04 +0300

    changes

--- questions/matrix.py
+++ questions/matrix.py
@@ -1,9 +1,11 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        # get the number of rows and columns 
         ROWS = len(matrix)
-        COLS = len(matrix[0])
+        COLS = len(matrix[0])#the matrix is always going to be non-empty that is it cant be zero
         
         top = 0
+        # the number of rows minus 1
         bot = ROWS -1
         
         while top <= bot:
@@ -19,14 +21,14 @@ class Solution:
             return False
         row = (top + bot)//2
         l = 0
+        # the position of r is the number of columns minus 1
         r = COLS - 1
         while l <= r:
             m = (l + r)//2
-            if target < matrix[row] [m]:
+            if target < matrix[row][m]:
                 r = m - 1
-            elif target > matrix[row] [m]:
+            elif target > matrix[row][m]:
                 l = m + 1
             else:
                 return True
         return False
-        
